152ac8b46059d4597739d380ea899c9df5e582f4
riggun <jonnyfase@mail.ru>
2020-10-29 13:32:31 +0300

    search_fix

--- blogitproject/pages/views.py
+++ blogitproject/pages/views.py
@@ -5,6 +5,8 @@ from profiles.models import Profile
 from taggit.models import Tag
 from django.core.paginator import Paginator
 # Create your views here.
+
+
 @login_required
 def index(request):
     blogs = Blog.objects.filter(is_published=True).order_by('-created_at')
@@ -12,12 +14,14 @@ def index(request):
     latest_blogs = blogs[:4]
 
     tags = Tag.objects.all()
+    get_params = ''  # параметр фильтрации и сортировки для Pagination
 
     if request.method == 'GET':
         search = request.GET.get('search', '')
         if search:
-            blogs = blogs.filter(text=search)
-        
+            blogs = blogs.filter(text__icontains=search)
+            get_params += '&search={0}'.format(search)
+
         sort_by = request.GET.get('sort_by', '')
         if sort_by:
             sort_by_dict = {
@@ -32,8 +36,8 @@ def index(request):
         filter_by_tag = request.GET.get('filter_by_tag', '')
         if filter_by_tag:
             blogs = blogs.filter(tags__name__icontains=filter_by_tag)
-    #Paginator
-    paginator = Paginator(blogs, 2) # Show 2 blogs per page.
+    # Paginator
+    paginator = Paginator(blogs, 2)  # Show 2 blogs per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -43,5 +47,5 @@ def index(request):
         'latest_blogs': latest_blogs,
         'tags': tags,
         'page_obj': page_obj,
-        }
+    }
     return render(request, 'pages/index.html', context=context)
